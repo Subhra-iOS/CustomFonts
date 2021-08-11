3dc326758bc4fd1af309dc1359d75ae9cb26765e
Subhra Roy <subhra.roy@e-arc.com>
2021-08-11 11:15:51 +0530

    add new project

---
+++ CustomFonts/Downloader.swift
@@ -0,0 +1,56 @@
+//
+//  Downloader.swift
+//  CustomFonts
+//
+//  Created by Subhra Roy on 22/07/21.
+//  Copyright © 2021 Subhra Roy. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+import CoreText
+import UIKit
+
+class Downloader {
+    class func load(URL: NSURL, closure: @escaping ((UIFont) -> Void)) -> Void{
+        let sessionConfig = URLSessionConfiguration.default
+        let session = URLSession(configuration: sessionConfig, delegate: nil, delegateQueue: nil)
+        let request = NSMutableURLRequest(url: URL as URL)
+        request.httpMethod = "GET"
+        let task = session.dataTask(with: request as URLRequest) { (data, response, error) -> Void in
+            if (error == nil) {
+                // Success
+                let statusCode = (response as! HTTPURLResponse).statusCode
+                print("Success: \(statusCode)")
+                
+                // This is your file-variable:
+                // data
+                                
+                if let fontData = data , let dataProvider: CGDataProvider = CGDataProvider(data: fontData as CFData), let cgFont = CGFont(dataProvider){
+                    var error: Unmanaged<CFError>?
+                    if !CTFontManagerRegisterGraphicsFont(cgFont, &error){
+                        print("Error loading Font!")
+                        DispatchQueue.main.async{
+                            closure(UIFont(name: "Arial", size: 30.0)!)
+                        }
+                    } else {
+                        if let fontName: String = cgFont.postScriptName as String?{
+                            DispatchQueue.main.async {
+                                if let uiFont : UIFont = UIFont(name: String(describing: fontName) , size: 30){
+                                    print("\(uiFont.familyName)")
+                                    closure(uiFont)
+                                }
+                            }
+                        }
+                    }
+                }
+                        
+            }
+            else {
+                // Failure
+                print("Faulure: %@", error?.localizedDescription ?? "");
+            }
+        }
+        task.resume()
+    }
+}
---
+++ CustomFonts/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  CustomFonts
+//
+//  Created by Subhra Roy on 22/07/21.
+//  Copyright © 2021 Subhra Roy. All rights reserved.
+//
+
+import UIKit
+
+struct FontsCollection {
+    private let fontsArray = ["http://www.webpagepublicity.com/free-fonts/e/Earthquake%20MF.ttf", //0
+                              "http://www.webpagepublicity.com/free-fonts/a/Alako-Bold.ttf", //1
+                              "http://www.webpagepublicity.com/free-fonts/a/Amelia.ttf", //2
+                              "http://www.webpagepublicity.com/free-fonts/a/Amazone%20BT.ttf"] //3
+    
+    subscript(index: Int) -> String{
+        return fontsArray[index]
+    }
+}
+
+class ViewController: UIViewController {
+    @IBOutlet weak var customFontLabel: UILabel!
+    var customFont: UIFont?
+    let fonts: FontsCollection = FontsCollection()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    
+        self.customFontLabel.text = "Hello World"
+    }
+    
+    @IBAction func pushNewFontButton(sender: Any) {
+        if let url = NSURL(string: fonts[2]) {
+                Downloader.load(URL: url) { [weak self] (font) in
+                    self?.customFont = font
+                }
+            }
+    }
+    
+    @IBAction func pushAndSetTheNewFont(sender: Any) {
+        guard let font = self.customFont  else {
+            return
+        }
+        self.customFontLabel.font = font
+    }
+ 
+
+}
+
